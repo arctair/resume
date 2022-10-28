2b2fb23536f921d883a12dc399847009a19a741b
tyler <tyler@cruftbusters.com>
2022-10-28 13:46:09 -0700

    fill in some details

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function App() {
   return (
     <div
@@ -33,26 +35,75 @@ export default function App() {
       <div>
         <h2>Experience</h2>
         <Experience
-          companyName="companyName"
-          jobTitle="jobTitle"
-          startDate="startDate"
-          endDate="endDate"
+          companyName="Best Buy"
+          jobTitle="Software Consultant"
+          startDate="August 2019"
+          endDate="October 2021"
+          description="description"
+        />
+        <Experience
+          companyName="Target"
+          jobTitle="Software Engineer"
+          startDate="November 2018"
+          endDate="August 2019"
           description="description"
         />
       </div>
       <div>
         <h2>Education</h2>
         <Education
-          school="school"
-          countryRegion="countryRegion"
-          stateProvince="stateProvince"
-          yearsAttended="yearsAttended"
-          fieldOfStudy="fieldOfStudy"
-          levelOfEducation="levelOfEducation"
+          school="North Dakota State University"
+          countryRegion="United States of America"
+          stateProvince="Fargo, North Dakota"
+          yearsAttended="2014-2017"
+          fieldOfStudy="B.S. Computer Science"
+          levelOfEducation="Undergraduate"
         />
       </div>
       <div>
         <h2>Areas of Expertise, Skills, and Relevant Courses</h2>
+        <Columns>
+          <TitledList
+            title="Practices"
+            elements={[
+              'Pair Programming',
+              'Test Driven Development',
+              'Incremental Design',
+              'Continuous Intergration and Deployment',
+              'Collective Ownership',
+              'Blameless Culture',
+              'Root-cause Analysis',
+            ]}
+          />
+          <TitledList
+            title="Technologies"
+            elements={[
+              'Java/JUnit',
+              'Groovy/Spock',
+              'Kotlin/Kotest',
+              'Typescript/Jest',
+              'Python/Pytest',
+              'React',
+              'Node',
+              'Bukkit/Spigot/Paper',
+              'Bungee/Velocity',
+              'Linux/Windows/MacOS',
+              'AWS (S3, EC2, Route53, Cloudfront, awscli+sdk)',
+              'IP/TCP/TLS/HTTP',
+              'DNS',
+            ]}
+          />
+          <div>
+            <h3>Courses</h3>
+            <Course
+              title="Leading Technical Change"
+              completionDate="July 21, 2022"
+              instructor="Michael Hill, Coach"
+              url="https://www.geepawhill.org/courses/leading-technical-change"
+              description="description"
+            />
+          </div>
+        </Columns>
       </div>
     </div>
   )
@@ -99,6 +150,53 @@ function Education({
     <div>
       {school} {countryRegion} {stateProvince} {yearsAttended}{' '}
       {fieldOfStudy} {levelOfEducation}
+    </div>
+  )
+}
+
+function Columns({ children }: React.PropsWithChildren) {
+  const childrenAsArray = React.Children.toArray(children)
+  return (
+    <div style={{ columnCount: 3 }}>
+      {childrenAsArray.map((child) => (
+        <div style={{ breakInside: 'avoid' }}>{child}</div>
+      ))}
+    </div>
+  )
+}
+
+interface TitledListProps {
+  title: string
+  elements: React.ReactNode[]
+}
+function TitledList({ title, elements }: TitledListProps) {
+  return (
+    <>
+      <h3>{title}</h3>
+      {elements.map((element) => (
+        <div>{element}</div>
+      ))}
+    </>
+  )
+}
+
+interface CourseProps {
+  title: string
+  completionDate: string
+  instructor: string
+  url: string
+  description: string
+}
+function Course({
+  title,
+  completionDate,
+  instructor,
+  url,
+  description,
+}: CourseProps) {
+  return (
+    <div>
+      {title} {completionDate} {instructor} {url} {description}
     </div>
   )
 }
