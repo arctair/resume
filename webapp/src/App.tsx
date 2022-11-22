55ec98cdc53e17c28d1d01fa405793e30f64eebb
tyler <tyler@cruftbusters.com>
2022-11-22 11:51:00 -0800

    fork out prime resume from mojang resume

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,25 @@
+import { Link, Route, Routes } from 'react-router-dom'
 import Mojang from './resumes/Mojang'
+import Prime from './resumes/Prime'
 
 export default function App() {
-  return <Mojang />
+  return (
+    <Routes>
+      <Route path="/mojang" element={<Mojang />} />
+      <Route path="/prime" element={<Prime />} />
+      <Route
+        path="*"
+        element={
+          <>
+            <Link to="/mojang" style={{ display: 'block' }}>
+              mojang
+            </Link>
+            <Link to="/prime" style={{ display: 'block' }}>
+              prime
+            </Link>
+          </>
+        }
+      />
+    </Routes>
+  )
 }
